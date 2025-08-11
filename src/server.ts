6127c3f23818358846e93d02c38c79710590d2d7
menezes0067 <menezes0067@gmail.com>
2025-08-11 16:05:58 -0300

    configuring category route

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import fastify, { FastifyInstance } from 'fastify';
 import UserRoute from './routes/user-route';
 import ClientRoute from './routes/client-route';
 import EmployeeRoute from './routes/employee-route';
+import CategoryRoute from './routes/category-route';
 
 const app: FastifyInstance = fastify({});
 
 app.register(UserRoute)
 app.register(ClientRoute)
 app.register(EmployeeRoute)
+app.register(CategoryRoute)
 
 app.listen({
     host: '0.0.0.0',

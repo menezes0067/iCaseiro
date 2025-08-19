eb03230c7881846e3b942d67a6b5d4a0a59eb5f6
menezes0067 <menezes0067@gmail.com>
2025-08-18 22:29:06 -0300

    refactor: refactoring menu schema

--- src/schemas/menu-schema.ts
+++ src/schemas/menu-schema.ts
@@ -1,3 +1,4 @@
+import { Decimal } from "@prisma/client/runtime/library";
 import z, { uuidv4 } from "zod";
 
 export const MenuSchema = z.object({
@@ -6,4 +7,16 @@ export const MenuSchema = z.object({
     description: z.string(),
     value: z.number(),
     category_id: z.string()
+})
+
+export const MenuSchemaConvert = z.object({
+    id: z.string().optional(),
+    name: z.string(),
+    description: z.string(),
+    value: z.number().transform((val) => new Decimal(val)),
+    category_id: z.string()
+})
+
+export const ParamIdSchema = z.object({
+  id: z.string()
 })

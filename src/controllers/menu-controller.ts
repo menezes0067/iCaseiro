9cad37723995fa14b57a00b29f44806b1083bf17
menezes0067 <menezes0067@gmail.com>
2025-08-18 22:28:44 -0300

    feat: menu route

--- src/controllers/menu-controller.ts
+++ src/controllers/menu-controller.ts
@@ -1,18 +1,18 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { MenuService } from "../services/menu-service";
 import { MenuRepositoryPrisma } from "../repositories/menu-repository";
-import { MenuSchema } from "../schemas/menu-schema";
+import { MenuSchema, MenuSchemaConvert, MenuSchemaParam, ParamIdSchema } from "../schemas/menu-schema";
 
 const menuRepository = new MenuRepositoryPrisma
 const menuService = new MenuService(menuRepository)
 
 export const CreateMenu = async (req: FastifyRequest, rep: FastifyReply) => {
     try {
-        const menuData = MenuSchema.parse(req.body)        
+        const menuData = MenuSchema.parse(req.body)
         const menu = await menuService.executeCreateMenu(menuData)
         rep.code(201).send(menu)
     } catch (error) {
-        console.log(error) 
+        console.log(error)
         rep.code(500).send({ message: 'Error in creation menu'})
     }
 }
@@ -24,6 +24,22 @@ export const GetMenu = async (req: FastifyRequest, rep: FastifyReply) => {
         rep.code(200).send(readMenuItem)
     } catch (error) {
         console.log(error)
-        rep.code(404).send({ message: ' not found '})   
+        rep.code(404).send({ message: ' not found '})
     }
+}
+
+export const UpdateItemMenu = async (req: FastifyRequest, rep: FastifyReply) => {
+  try {
+    const { id } = ParamIdSchema.parse(req.params)
+    const body = MenuSchemaConvert.parse(req.body)
+
+    const updateItem = await menuService.updateItemMenu(id, body)
+
+    console.log(updateItem)
+
+    rep.code(200).send(updateItem)
+  }catch(error) {
+    console.log(error)
+    rep.code(500).send({ error:'Error in update' })
+  }
 }
--- src/routes/menu-route.ts
+++ src/routes/menu-route.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance } from "fastify";
-import { CreateMenu, GetMenu } from "../controllers/menu-controller";
+import { CreateMenu, GetMenu, UpdateItemMenu } from "../controllers/menu-controller";
 
 export default function MenuRoute(fastify: FastifyInstance) {
   fastify.post("/menu", CreateMenu);
   fastify.get("/menu", GetMenu);
+  fastify.put("/menu/:id", UpdateItemMenu)
 }
